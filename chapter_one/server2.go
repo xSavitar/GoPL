b70bcfabadf21a5af31d2d24451a06d2f5377f0d
Alangi Derick <alangiderick@gmail.com>
2017-08-14 00:10:33 +0100

    Server to print URL component and counter
    * Using Mutexes and priting URL component of a
      proveded URL.
    
    * Count the number of calls so far :)
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_one/server2.go
@@ -0,0 +1,35 @@
+// Author: Alangi Derick
+// Description: Server1 is a minimal "echo" and counter server.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the requested URL
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock() // lock mutex
+	count++
+	mu.Unlock() // unlock mutex
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// counter echoes the number of calls so far.
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock() // lock mutex
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
