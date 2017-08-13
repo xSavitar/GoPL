f9c113c1dedcb7f7ac335eb86f8e1f04f2fe7936
Alangi Derick <alangiderick@gmail.com>
2017-08-13 21:23:51 +0100

    Get URL content of a page, HTML dump
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_one/fetch.go
@@ -0,0 +1,28 @@
+// Author: Alangi Derick
+// Description: Fetch prints the content found at a URL provided
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url) // Get the URL
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body) // assigned content into "b"
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
