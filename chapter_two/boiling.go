c23cd2531d77b28b25c7255584e43436528b6bdd
Alangi Derick <alangiderick@gmail.com>
2017-08-14 11:24:35 +0100

    Boiling point in °F or °C
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_two/boiling.go
@@ -0,0 +1,17 @@
+// Author: Alangi Derick
+// Description: Boiling prints the boiling point of water.
+
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main() {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Print("boiling point = %g°F or %g°C\n", f, c) // the ° on Mac is done by holding "Option + Shift + 8"
+
+	// Output:
+	// boiling point = 212°F or 100°C
+}
