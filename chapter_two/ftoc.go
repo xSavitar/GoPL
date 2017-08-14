94ba877e96337d205c9a6e0b1607c608e20face0
Alangi Derick <alangiderick@gmail.com>
2017-08-14 17:47:00 +0100

    Formatting the ftoc.go file
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- chapter_two/ftoc.go
+++ chapter_two/ftoc.go
@@ -9,7 +9,7 @@ func main() {
 	const freezingF, boilingF = 32.0, 212.0
 
 	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
-	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF)) // "212°F =  100°C"
+	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))   // "212°F =  100°C"
 }
 
 // defining the conversion function
