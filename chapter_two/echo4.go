d6439c4400daaea138bcb61cc3ec0cf06c129f15
Alangi Derick <alangiderick@gmail.com>
2017-08-14 18:28:30 +0100

    Echo with flags :)
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_two/echo4.go
@@ -0,0 +1,23 @@
+// Author: Alangi Derick
+// Description: Echo4 prints its command-line arguments.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+// the flags allowed :)
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+
+	if !*n {
+		fmt.Println()
+	}
+}
