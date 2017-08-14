4b58a0a1945e3ac3580d9be948ada86a957447f0
Alangi Derick <alangiderick@gmail.com>
2017-08-14 11:25:39 +0100

    Formatting boiling.go
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- chapter_two/boiling.go
+++ chapter_two/boiling.go
@@ -10,7 +10,7 @@ const boilingF = 212.0
 func main() {
 	var f = boilingF
 	var c = (f - 32) * 5 / 9
-	fmt.Print("boiling point = %g°F or %g°C\n", f, c) // the ° on Mac is done by holding "Option + Shift + 8"
+	fmt.Printf("boiling point = %g°F or %g°C\n", f, c) // the ° on Mac is done by holding "Option + Shift + 8"
 
 	// Output:
 	// boiling point = 212°F or 100°C
