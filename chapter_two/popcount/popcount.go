0297f387625973ea2881b61492c9a9ee0ba7fc86
Alangi Derick <alangiderick@gmail.com>
2017-08-15 19:25:43 +0100

    Formatting popcount.go
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- chapter_two/popcount/popcount.go
+++ chapter_two/popcount/popcount.go
@@ -13,12 +13,12 @@ func init() {
 
 // PopCount returns the population count (number of set bits) of x.
 func PopCount(x uint64) int {
-	return int(pc[byte(x>>(0*8))] + 
-		pc[byte(x>>(1*8))] + 
-		pc[byte(x>>(2*8))] + 
-		pc[byte(x>>(3*8))] + 
-		pc[byte(x>>(4*8))] + 
-		pc[byte(x>>(5*8))] + 
-		pc[byte(x>>(6*8))] + 
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
 		pc[byte(x>>(7*8))])
 }
