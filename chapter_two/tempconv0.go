a151f1eb09fb46da13134d88937b2fae8a41daeb
Alangi Derick <alangiderick@gmail.com>
2017-08-15 18:46:09 +0100

    Formatting the package
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- chapter_two/tempconv0.go
+++ chapter_two/tempconv0.go
@@ -11,12 +11,12 @@ type Fahrenheit float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
-	FreezingC	  Celsius = 0
+	FreezingC     Celsius = 0
 	Boiling       Celsius = 100
 )
 
 // Celsius to Fahrenheit
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c * 9 / 5 + 32) }
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // Fahrenheit to Celsius
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
