35ecaaea74a267556747ed081be71a63d4ffcb38
Alangi Derick <alangiderick@gmail.com>
2017-08-31 21:07:00 +0100

    Appending elements to a slice
    This code appends integer elements to a slice
    and does basic operations like grow the slice if
    there is no sufficient space in the slice.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_4/appendInt.go
@@ -0,0 +1,37 @@
+// Author: Alangi Derick N
+// Append integers User Defined Function (UDF)
+
+package main
+
+import "fmt"
+
+// Append integers to a slice
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		// there is room to grow. Extend the slice.
+		z = x[:zlen]
+	} else {
+		// There is insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized linear complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x) // a built-in function; see text
+	}
+	z[len(x)] = y
+	return z
+}
+
+// main function definition
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d    cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+}
