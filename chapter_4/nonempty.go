1682bb3cdd4b36bc765e2a89e311948c39c0c036
Alangi Derick <alangiderick@gmail.com>
2017-08-31 21:32:10 +0100

    Checking for non empty strings
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_4/nonempty.go
@@ -0,0 +1,26 @@
+// Author: Alangi Derick N
+// Nonempty is an example of an in-place slice algorithm.
+
+package main
+
+import "fmt"
+
+// nonempty returns a slice holding only the non-empty strings.
+// The underlying array is modified during the call.
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
+
+// main function definition
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data)) // `["one" "three"]`
+	fmt.Printf("%q\n", data)           // `["one" "three" "three"]`
+}
