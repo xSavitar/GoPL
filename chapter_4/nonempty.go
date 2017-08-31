420c24658e682da104ee9e06167788b24e4880e7
Alangi Derick <alangiderick@gmail.com>
2017-08-31 21:41:50 +0100

    using append() to perform non-empty
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- chapter_4/nonempty.go
+++ chapter_4/nonempty.go
@@ -18,9 +18,29 @@ func nonempty(strings []string) []string {
 	return strings[:i]
 }
 
+// Doing non-empty using append()
+func nonemptyAppend(strings []string) []string {
+	out := strings[:0] // zero-length slice of original
+	for _, s := range strings {
+		if s != "" {
+			out = append(out, s)
+		}
+	}
+	return out
+}
+
 // main function definition
 func main() {
 	data := []string{"one", "", "three"}
+
+	// using the define nonempty()
+	fmt.Printf("Using the default nonempty() function\n")
 	fmt.Printf("%q\n", nonempty(data)) // `["one" "three"]`
 	fmt.Printf("%q\n", data)           // `["one" "three" "three"]`
+
+	info := []string{"one", "", "three"}
+	// using the append function
+	fmt.Printf("\nUsing the append() version of nonempty()\n")
+	fmt.Printf("%q\n", nonemptyAppend(info))
+	fmt.Printf("%q\n", info)
 }
